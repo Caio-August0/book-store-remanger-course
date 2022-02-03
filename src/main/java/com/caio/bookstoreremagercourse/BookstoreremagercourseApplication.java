e54e456a68a5a72e0f267c870d2061101d9f5790
Caio Augusto <caioaugustobr0106@gmail.com>
2022-02-03 09:08:23 -0300

    Projeto criado

---
+++ src/main/java/com/caio/bookstoreremagercourse/BookstoreremagercourseApplication.java
@@ -0,0 +1,13 @@
+package com.caio.bookstoreremagercourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreremagercourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreremagercourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caio/bookstoreremagercourse/BookstoreremagercourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caio.bookstoreremagercourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreremagercourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
